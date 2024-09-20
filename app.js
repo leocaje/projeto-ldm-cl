b88d21c1b0eb20b1edd4d3b5c8a09f5aa933c0c4
LeoCaje <leoacaje@gmail.com>
2024-09-19 21:31:38 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const app = express()
+const handlebars = require("express-handlebars").engine
+const bodyParser = require("body-parser")
+
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app')
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore')
+
+const serviceAccount = require("./serviceAccountKey.json")
+
+initializeApp({
+    credential: cert(serviceAccount)
+})
+
+const db = getFirestore()
+
+app.engine("handlebars", handlebars({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get("/", function(req, res) {
+    res.render("primeira_pagina")
+})
+
+app.post("/cadastrar", function(req, res) {
+    var result = db.collection("clientes").add({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao
+    }).then(function() {
+        console.log("Dados cadastrados com sucesso!")
+    })
+})
+
+app.listen(8081, function() {
+    console.log("Servidor ativo!")
+})
